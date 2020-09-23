4cc9302694ae1c833d5e5540014fc2bbf84981d2
lordtomer <lordtomer22@gmail.com>
2020-09-23 19:51:19 +0300

    update
    yes

--- socot/src/main/java/com/Tomer_and_more/sucot/armor/ModArmorMaterial.java
+++ socot/src/main/java/com/Tomer_and_more/sucot/armor/ModArmorMaterial.java
@@ -1,45 +1,75 @@
 package com.Tomer_and_more.sucot.armor;
 
+import com.Tomer_and_more.sucot.sucot;
+import com.Tomer_and_more.sucot.util.RegistryHandler;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
 
 public enum ModArmorMaterial implements IArmorMaterial {
-    ;
+
+    HILOK(sucot.MOD_ID + ":hilok", 50, new int[] {100, 0, 0, 0}, 50,
+            SoundEvents.AMBIENT_CAVE, 40.0f, () -> {return Ingredient.fromItems(RegistryHandler.SUCA_WALL_ITEM.get())});
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[] {11, 16, 15, 13};
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageRedactionArray;
+    private final int enchantAbility;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final Supplier<Ingredient> repairMaterial;
+
+    ModArmor(String name, int maxDamageFactor, int[] damageRedactionArray,
+             int enchantAbility, SoundEvent soundEvent, float toughness, Supplier<Ingredient> repairMaterial){
+        this.name = name;
+        this.maxDamageFactor = maxDamageFactor;
+        this.damageRedactionArray = damageRedactionArray;
+        this.enchantAbility = enchantAbility;
+        this.soundEvent = soundEvent;
+        this.toughness = toughness;
+        this.repairMaterial = repairMaterial;
+    }
 
     @Override
     public int getDurability(EquipmentSlotType slotIn) {
-        return 0;
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
     }
 
     @Override
     public int getDamageReductionAmount(EquipmentSlotType slotIn) {
-        return 0;
+        return this.damageRedactionArray[slotIn.getIndex()] * 100;
     }
 
     @Override
     public int getEnchantability() {
-        return 0;
+        return enchantAbility;
     }
 
     @Override
     public SoundEvent getSoundEvent() {
-        return null;
+        return this.soundEvent;
     }
 
     @Override
     public Ingredient getRepairMaterial() {
-        return null;
+        return this.repairMaterial.get();
     }
 
+    @OnlyIn(Dist.CLIENT)
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 
     @Override
     public float getToughness() {
-        return 0;
+        return this.toughness;
     }
 }
--- socot/src/main/java/com/Tomer_and_more/sucot/util/RegistryHandler.java
+++ socot/src/main/java/com/Tomer_and_more/sucot/util/RegistryHandler.java
@@ -1,10 +1,13 @@
 package com.Tomer_and_more.sucot.util;
 
+import com.Tomer_and_more.sucot.armor.ModArmorMaterial;
 import com.Tomer_and_more.sucot.blocks.BlockItemBase;
 import com.Tomer_and_more.sucot.blocks.suca_wall;
 import com.Tomer_and_more.sucot.items.ItemBase;
 import com.Tomer_and_more.sucot.sucot;
 import net.minecraft.block.Block;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -23,10 +26,14 @@ public class RegistryHandler {
     }
 //ITEMS
     public static final RegistryObject<Item> LULAV = ITEMS.register("lulav", ItemBase::new);
+    public static final RegistryObject<Item> ATROG = ITEMS.register("atrog", ItemBase::new);
 
 //BLOCKS
     public static final RegistryObject<Block> SUCA_WALL = BLOCKS.register("suca_wall", suca_wall::new);
 
 //BLOCK ITEMS
     public static final RegistryObject<Item> SUCA_WALL_ITEM = ITEMS.register("suca_wall", () -> new BlockItemBase(SUCA_WALL.get()));
+//KIPA
+    RegistryObject<ArmorItem> HILOK = ITEMS.register("hilok", () -> new ArmorItem(ModArmorMaterial.HILOK,
+        EquipmentSlotType.HEAD, new Item.Properties().group(sucot.TAB)));
 }
